7de15cfee26e49e211f5829ac7d9cd2ae8855640
tsugunobukumamoto <jambokumamoto@gmail.com>
2020-06-12 09:44:47 +0000

    gem追加&view作成 全体基本構造作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  # ログイン機能(sessionコントローラ)
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+      get 'attendances/edit_one_month' 
+      patch 'attendances/update_one_month'
+    end
+    resources :attendances, only: :update
+  end
 end
